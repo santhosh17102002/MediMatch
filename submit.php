23dff482f40b467c7adedb8f76eaaa445c5a257e
santhosh17102002 <109502221+santhosh17102002@users.noreply.github.com>
2023-10-10 22:23:49 +0530

    hi

--- submit.php
+++ submit.php
@@ -1,7 +1,7 @@
 <?php
     $db_hostname = "127.0.0.1";
     $db_username = "root";
-    $db_password = "";
+    $db_password = "root";
     $db_name = "Medimatch";
     $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
     if (!$conn) {
