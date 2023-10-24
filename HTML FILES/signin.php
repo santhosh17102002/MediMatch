b71f95c98d75abe37011aba77f6d970b831145fc
santhosh17102002 <109502221+santhosh17102002@users.noreply.github.com>
2023-10-24 20:29:43 +0530

    added about us to many pages

--- HTML FILES/signin.php
+++ HTML FILES/signin.php
@@ -1,13 +1,30 @@
 <?php
     $db_hostname = "127.0.0.1";
     $db_username = "root";
-    $db_password = "root";
-    $db_name = "Medimatch";
+    $db_password = "";
+    $db_name = "test";
     $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
     if (!$conn) {
         echo "Connection failed: " . mysqli_connect_error();
         exit;
     }
 
+    $Email = $_POST['Email'];
+    $password = $_POST['password'];
 
+    $sql = "SELECT * FROM LOGIN WHERE Email='$Email' AND password='$password'";
+    $result = mysqli_query($conn, $sql);
+    if (!$result) {
+        echo "Error: " . mysqli_error($conn);
+        exit;
+    }
+    if (mysqli_num_rows($result) > 0) {
+        header("Location: ../index.html");
+        
+    } else {
+        header("Location: login.html");
+        
+    }
+
+    mysqli_close($conn);
 ?>
--- HTML FILES/submit.php
+++ HTML FILES/submit.php
@@ -1,8 +1,8 @@
 <?php
     $db_hostname = "127.0.0.1";
     $db_username = "root";
-    $db_password = "root";
-    $db_name = "Medimatch";
+    $db_password = "";
+    $db_name = "test";
     $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
     if (!$conn) {
         echo "Connection failed: " . mysqli_connect_error();
